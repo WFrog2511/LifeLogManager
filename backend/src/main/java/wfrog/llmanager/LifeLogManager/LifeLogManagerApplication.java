f8d5181159d7de30727c5dabea4e04efec3395b6
WFrog2511 <yadokugaeru@outlook.jp>
2023-09-15 02:24:31 +0900

    🎉 Initialize the spring boot backend

---
+++ backend/src/main/java/wfrog/llmanager/LifeLogManager/LifeLogManagerApplication.java
@@ -0,0 +1,13 @@
+package wfrog.llmanager.LifeLogManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeLogManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeLogManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/wfrog/llmanager/LifeLogManager/LifeLogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package wfrog.llmanager.LifeLogManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeLogManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
