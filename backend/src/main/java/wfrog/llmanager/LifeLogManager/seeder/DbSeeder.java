10858dfd8148394ea5109d5e581d585d101c63d6
WFrog2511 <yadokugaeru@outlook.jp>
2023-11-12 18:06:20 +0900

    :green_heart:テスト用のユーザーデータを自動登録する処理を追加

---
+++ backend/src/main/java/wfrog/llmanager/LifeLogManager/seeder/DbSeeder.java
@@ -0,0 +1,35 @@
+package wfrog.llmanager.LifeLogManager.seeder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import wfrog.llmanager.LifeLogManager.repository.UserRepository;
+import wfrog.llmanager.LifeLogManager.domain.User;
+
+@Component
+public class DbSeeder implements CommandLineRunner {
+    private final UserRepository userRepository;
+
+    public DbSeeder(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setPassword("password");
+        user1.setUsername("Tom");
+
+        User user2 = new User();
+        user1.setId(2L);
+        user2.setPassword("password");
+        user2.setUsername("Bob");
+
+        // すでにデータが存在していないかチェック
+        if (userRepository.findAll().isEmpty()) {
+            userRepository.save(user1);
+            userRepository.save(user2);
+        }
+    }
+}
